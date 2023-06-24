5affb8fe14327853e7850dd9771a98159e7791c3
Alexey <volodalexey@gmail.com>
2023-06-24 15:10:14 +0300

    EP3: keyboard input and player movement and enemies spawn

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
 use bevy::{
     prelude::{
-        default, App, AssetServer, Camera2dBundle, Commands, Component, Query, Res, Transform, With,
+        default, App, AssetServer, Camera2dBundle, Commands, Component, Input, KeyCode, Query, Res,
+        ScanCode, Transform, Vec3, With,
     },
     sprite::SpriteBundle,
+    time::Time,
     window::{PrimaryWindow, Window},
     DefaultPlugins,
 };
+use rand::random;
 
+pub const PLAYER_SPEED: f32 = 500.0;
+pub const PLAYER_SIZE: f32 = 64.0; // This is the player sprite size.
+pub const NUMBER_OF_ENEMIES: usize = 4;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_player)
+        .add_startup_system(spawn_enemies)
+        .add_system(player_movement)
+        .add_system(confine_player_movement)
         .run();
 }
 
 #[derive(Component)]
 pub struct Player {}
+
+#[derive(Component)]
+pub struct Enemy {}
 
 pub fn spawn_player(
     mut commands: Commands,
@@ -42,4 +54,116 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
         ..default()
     });
+}
+
+pub fn spawn_enemies(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+) {
+    let window = window_query.get_single().unwrap();
+
+    for _ in 0..NUMBER_OF_ENEMIES {
+        let random_x = random::<f32>() * window.width();
+        let random_y = random::<f32>() * window.height();
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {},
+        ));
+    }
+}
+pub enum EScanCode {
+    W = 17,
+    A = 30,
+    S = 31,
+    D = 32,
+    Left = 105,
+    Right = 106,
+    Up = 103,
+    Down = 108,
+}
+
+pub fn player_movement(
+    keyboard_input_scan_code: Res<Input<ScanCode>>,
+    keyboard_input_key_code: Res<Input<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut direction = Vec3::ZERO;
+
+        if keyboard_input_scan_code.any_pressed([
+            ScanCode(EScanCode::A as u32),
+            ScanCode(EScanCode::Left as u32),
+        ]) || keyboard_input_key_code.any_pressed([KeyCode::A, KeyCode::Left])
+        {
+            direction += Vec3::new(-1.0, 0.0, 0.0)
+        }
+
+        if keyboard_input_scan_code.any_pressed([
+            ScanCode(EScanCode::D as u32),
+            ScanCode(EScanCode::Right as u32),
+        ]) || keyboard_input_key_code.any_pressed([KeyCode::D, KeyCode::Right])
+        {
+            direction += Vec3::new(1.0, 0.0, 0.0);
+        }
+        if keyboard_input_scan_code.any_pressed([
+            ScanCode(EScanCode::W as u32),
+            ScanCode(EScanCode::Up as u32),
+        ]) || keyboard_input_key_code.any_pressed([KeyCode::W, KeyCode::Up])
+        {
+            direction += Vec3::new(0.0, 1.0, 0.0);
+        }
+        if keyboard_input_scan_code.any_pressed([
+            ScanCode(EScanCode::S as u32),
+            ScanCode(EScanCode::Down as u32),
+        ]) || keyboard_input_key_code.any_pressed([KeyCode::S, KeyCode::Down])
+        {
+            direction += Vec3::new(0.0, -1.0, 0.0);
+        }
+
+        // if direction.length() > 0.0 {
+        //     direction = direction.normalize();
+        // }
+        direction = direction.normalize_or_zero();
+
+        transform.translation += direction * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size = PLAYER_SIZE / 2.0; // 32.0
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player_transform.translation;
+
+        // Bound the player x position
+        if translation.x < x_min {
+            translation.x = x_min;
+        } else if translation.x > x_max {
+            translation.x = x_max;
+        }
+        // Bound the players y position.
+        if translation.y < y_min {
+            translation.y = y_min;
+        } else if translation.y > y_max {
+            translation.y = y_max;
+        }
+
+        player_transform.translation = translation;
+    }
 }
