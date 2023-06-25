52643e2af722a5232e19df4dd8a376ca9083903f
Alexey <volodalexey@gmail.com>
2023-06-25 14:47:41 +0300

    EP8 - Explicit system set

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::{App, Plugin};
+use bevy::prelude::{App, IntoSystemConfig, IntoSystemSetConfig, Plugin, SystemSet};
 
 use self::systems::{
     confine_player_movement, enemy_hit_player, player_hit_star, player_movement, spawn_player,
@@ -7,13 +7,24 @@ use self::systems::{
 pub mod components;
 mod systems;
 
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Debug, Hash, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
-            .add_system(player_movement)
-            .add_system(confine_player_movement)
+        app
+            // Configure System Sets
+            .configure_set(MovementSystemSet.before(ConfinementSystemSet))
+            // Startup Systems
+            .add_startup_system(spawn_player)
+            // Systems
+            .add_system(player_movement.in_set(MovementSystemSet))
+            .add_system(confine_player_movement.in_set(ConfinementSystemSet))
             .add_system(enemy_hit_player)
             .add_system(player_hit_star);
     }
